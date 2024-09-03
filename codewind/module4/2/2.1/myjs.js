e296ced581ada92a67090394eefe07f4a764e41f
root <root@DobrIdnya>
2024-09-03 14:49:36 +0300

    Добавлены новые задания + описания старых

---
+++ codewind/module4/2/2.1/myjs.js
@@ -0,0 +1,8 @@
+let student = { name: "Tony", age: "16", averageGrade: "3.5" };
+console.log('Студент с именем ', student.name,
+    ', возраст которого ', student.age,
+    ' лет, имеет средний балл: ', student.averageGrade, '.');
+
+student.averageGrade++;
+
+console.log('Его новый балл: ', student.averageGrade, '.');
