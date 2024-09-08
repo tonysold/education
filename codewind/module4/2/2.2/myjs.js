10997ec3192f31e4dc03b45ede0c191b0776cdcc
root <root@DobrIdnya>
2024-09-08 19:23:51 +0300

    Добавлены новые задания

---
+++ codewind/module4/2/2.2/myjs.js
@@ -0,0 +1,8 @@
+let str = '12345';
+let last = str.length - 1;
+if (str[last] === '5') {
+    console.log('true');
+}
+else {
+    console.log ('не тру');
+}
---
+++ codewind/module4/2/2.4/myjs.js
@@ -0,0 +1,7 @@
+let arr = [1, 2, 3, 4, 5];
+
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    
+    document.write(element,'<br>','<br>');
+}
